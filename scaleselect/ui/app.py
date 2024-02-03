b6e677dacfb9b1953567db906143a605e06643b0
Gregg Oliva <gloliva@proton.me>
2024-02-02 20:38:23 -0800

    Rename main app

--- scaleselect/ui/app.py
+++ scaleselect/ui/app.py
@@ -1,23 +1,24 @@
 """
 Author: Gregg Oliva
 """
-# stdlib imports
-import os
-
 # 3rd-party imports
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 
 # project imports
+from scales import ScaleBuilder
 from ui.interface import HomePage
 from ui.styles import get_css_files
 
 
-class ScaleSelect(App[int]):
+class ScaleZ(App[int]):
     CSS_PATH = get_css_files()
     BINDINGS = [
         Binding("q", "quit_app", "Quit App")
     ]
+
+    def setup(self) -> None:
+        self.scale_builder = ScaleBuilder()
 
     def compose(self) -> ComposeResult:
         yield HomePage()
